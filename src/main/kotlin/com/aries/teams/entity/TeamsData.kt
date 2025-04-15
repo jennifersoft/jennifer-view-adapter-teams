12e224777d0054e5bce367947d338fd4fd5d534a
alvin <alvin@jennifersoft.com>
2025-04-15 10:47:07 +0900

    예외 처리 #4

--- src/main/kotlin/com/aries/teams/entity/TeamsData.kt
+++ src/main/kotlin/com/aries/teams/entity/TeamsData.kt
@@ -17,7 +17,9 @@ class TeamsData(
 ) : JSONObject() {
     init {
         this.addDefaultInfo()
-        this.addAttachments()
+        if (prop.shareUrl != null && event.txid != -1L && event.txid != 0L) {
+            this.addAttachments()
+        }
     }
 
     private fun addDefaultInfo() {
@@ -33,26 +35,24 @@ class TeamsData(
     }
 
     private fun addAttachments() {
-        if (prop.shareUrl != null && event.txid != -1L && event.txid != 0L) {
-            val popupUrl = "/popup/xviewAnalysisV2?domainId=" + event.domainId +
-                    "&transactionId=" + event.txid + "&searchTime=" + event.time
-            val link = prop.shareUrl + popupUrl + "&redirect=" + encodeURIComponent(popupUrl)
+        val popupUrl = "/popup/xviewAnalysisV2?domainId=" + event.domainId +
+                "&transactionId=" + event.txid + "&searchTime=" + event.time
+        val link = prop.shareUrl + popupUrl + "&redirect=" + encodeURIComponent(popupUrl)
 
-            val targets = JSONArray()
-            val target = JSONObject()
-            target.put("os", "default")
-            target.put("uri", link)
-            targets.put(target)
+        val targets = JSONArray()
+        val target = JSONObject()
+        target.put("os", "default")
+        target.put("uri", link)
+        targets.put(target)
 
-            val attachments = JSONArray()
-            val obj = JSONObject()
-            obj.put("@type", "OpenUri")
-            obj.put("name", "View Transaction (JENNIFER5 Pop-Up)")
-            obj.put("targets", targets)
-            attachments.put(obj);
+        val attachments = JSONArray()
+        val obj = JSONObject()
+        obj.put("@type", "OpenUri")
+        obj.put("name", "View Transaction (JENNIFER5 Pop-Up)")
+        obj.put("targets", targets)
+        attachments.put(obj);
 
-            this.put("potentialAction", attachments)
-        }
+        this.put("potentialAction", attachments)
     }
 
     companion object {
